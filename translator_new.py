ad2c67cc4346eb2b876d23d9c0daa0fca8ecb338
Indira Dutta <66861242+indiradutta@users.noreply.github.com>
2021-08-02 15:16:30 +0530

    added comments

--- translator_new.py
+++ translator_new.py
@@ -6,30 +6,39 @@ import speech_recognition as sr
  
 class Translator(object):
     def __init__(self, lang='hi'):
+   
+        ''' lang : The language to which the speech needs to be translated (default : Hindi) '''
+        ''' output : The translated text '''
         self.lang = lang
         self.output = None
-
-    def translate(self, lang):
-
+    
+    ''' function for translating the speech '''
+    def translate(self, lang='hi'):
+        
+        ''' function for text to text translation '''
         def text_translate(line):
             blob = TextBlob(line)
             translated_sentence = blob.translate(to = lang)
             translated_sentence = str(translated_sentence)
             return translated_sentence
+           
+        ''' user input through microphone '''
         recognizer = sr.Recognizer()
         with sr.Microphone() as inputs:
             print("Please speak now")
             listening = recognizer.listen(inputs)
             print("Analysing...")
-
+            
+            ''' language recognization and speech to text conversion '''
             try:
                 b=recognizer.recognize_google(listening)
                 b=str(b) 
             except:
                 print("please speak again")
-
+        
         self.output = text_translate(b)
-
+        
+        ''' text to speech conversion '''
         obj = gTTS(text=self.output, lang="hi", slow=False)
         obj.save("translation.mp3")
         playsound('translation.mp3')
