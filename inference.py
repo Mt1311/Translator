c97a64292ef0377823d636bec3544a98e0451afd
Indira Dutta <66861242+indiradutta@users.noreply.github.com>
2021-08-02 15:24:34 +0530

    minor fixes

--- inference.py
+++ inference.py
@@ -1,4 +1,9 @@
 from translator_new import Translator
 
-lang = input('Enter the language to which the input needs to be translated: ')
-Translator.translate(Translator, lang=lang)
+choice = int(input('Do you want the translated speech to be in Hindi or in any other language? Enter 1 if it is Hindi and enter 0 if it is any other language: '))
+               
+if choice == 1:
+  Translator.translate(Translator, lang='hi')
+else:
+  lang = input('Enter the language to which the input needs to be translated: ')
+  Translator.translate(Translator, lang=lang)
